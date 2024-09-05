537a1397e860a41d75c0a2cf66c970de6bd7cc1a
Mått Hëñry <Matt.Henry@fisglobal.com>
2024-09-05 09:59:50 -0600

    Initial commit

---
+++ src/main/java/com/example/mail_healthcheck_prevents_start/MailHealthcheckPreventsStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.mail_healthcheck_prevents_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailHealthcheckPreventsStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailHealthcheckPreventsStartApplication.class, args);
+	}
+
+}
